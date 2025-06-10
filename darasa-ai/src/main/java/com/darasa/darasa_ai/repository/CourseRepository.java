716c971aaf6f1d9a8fd30a1a728eb1556634fecd
John <johnruge@uchicago.edu>
2025-06-10 11:26:01 -0500

    fix: pushing CourseRepository

--- darasa-ai/src/main/java/com/darasa/darasa_ai/repository/CourseRepository.java
+++ darasa-ai/src/main/java/com/darasa/darasa_ai/repository/CourseRepository.java
@@ -1,8 +1,12 @@
 package com.darasa.darasa_ai.repository;
 
 import com.darasa.darasa_ai.model.Course;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    //no custom methods
+    Optional<Course> findByCode(String code);
+    Optional<Course> findByName(String name);
 }
